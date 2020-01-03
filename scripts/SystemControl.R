1a63d0ecf95a0663d3b67ef71d4df2412361ffa2
Excel Que <34141973+exsquire@users.noreply.github.com>
2020-01-03 00:06:00 -0800

    General improvements
    Time, optimizer, comments

--- scripts/SystemControl.R
+++ scripts/SystemControl.R
@@ -1,17 +1,17 @@
 #System Control - running system control assumes that the array job is functional
 #I.E. properly parallelized, paths to inputs and outputs correct, first array check passed
 library(lubridate)
-
 #chmod the permuLite R and bash code
 system("chmod 755 permuLite_Rcode.R permuLite_run.sh")
 
-#Check for empty folders
+#Log folder must be empty
 stopifnot(length(list.files("../log")) == 0 )
-stopifnot(length(list.files("../results")) == 0)
 
 #Run 10 array jobs
-system("sbatch --array=1-10 permuLite_run.sh")
+system("sbatch -c 10 --array=1-10 permuLite_run.sh")
 
+
+#Check for 20 out/err files in the log folder
 #Wait loop
 cat("\nWaiting for jobs to finish, updates every minute...")
 while(length(list.files("../results")) != 10){
@@ -59,13 +59,13 @@ saveRDS(res, "../processed/profOut.rds")
 #OPTIMIZATION INCORPORTATES SAFETY MARGINS IN RESOURCE REQUESTS
 #Calculate optimal time  
 #For completed runs only
-isComp <- res[,"State"] == "COMPLETED"
+isComp <- res[,"State"] == "COMPLETED" | res[,"State"] == "RUNNING"
 pullTime <- strptime(res[isComp,"Used walltime"],'%H:%M:%S')
 #convert to strptime
 sec <- (pullTime$hour * 3600) + (pullTime$min * 60) + pullTime$sec
 
-#Request twice the average + 3sd time
-secForm <- seconds_to_period(ceiling(2*(mean(sec) + 3*sd(sec))))
+#Request twice the max
+secForm <- seconds_to_period(ceiling(2*(max(sec))))
 optTime <- sprintf('%02d:%02d:%02d', secForm@hour, minute(secForm), second(secForm))
 
 #Calculate optimal memory
@@ -73,38 +73,31 @@ optTime <- sprintf('%02d:%02d:%02d', secForm@hour, minute(secForm), second(secFo
 stopifnot(all(grepl("G", res[isComp,"Max Mem used"])))
 stopifnot(length(unique(res[isComp,"Cores"])) == 1)
 
-#Pull mem and cores and calc a "safe" value
-cores <- as.numeric(res[isComp,"Cores"][1])
+#Pull mem and calc a "safe" value - 120% + 4sd
 pullMem <- as.numeric(gsub("[A-Z].*$","",res[isComp,"Max Mem used"]))
-optMem <- 1.5 * (max(pullMem)) + 4*sd(pullMem)
-#round up to nearest half gig
-optMem_perCore <- (ceiling(optMem * 2)/2)/cores * 1000
+optMem <- 1.2*(max(pullMem)) + 4*sd(pullMem)
 
 #Adjust cores so optMem_perCore is close to max mem per core
 #But not over max mem per core
 #2500M is partition specific - enter your own partition's max if different
 adjustCores <- 2
-newCoreMem <- optMem_perCore
+#round up to nearest half gig
+optMem_perCore <- (ceiling(optMem * 2)/2)/adjustCores * 1000
 #If 2 cores are enough, keep, but update mem request
 #Else, increase cores and update mem request
-while(newCoreMem > 2500){
-  if(newCoreMem > 2500){
-    cat("Optimal memory request per core exceeds limit...\n")
+if(optMem_perCore <=2500){
+  optMem_perCore <- 2500
+}else{
+  while(optMem_perCore > 2500){
+    adjustCores <- adjustCores + 1
+    optMem_perCore <- ceiling((ceiling(optMem * 2)/2)/adjustCores * 1000)
   }
-  adjustCores <- adjustCores + 1
-  cat("Adjusting core allocation...\n")
-  newCoreMem <- ceiling((ceiling(optMem * 2)/2)/adjustCores * 1000)  
 }
-cat("Optimal Memory per Core =", newCoreMem,
-    "\nOptimal Cores =", adjustCores,
-    "\nOptimal Runtime =", optTime,"\n")
-
-cat("Submitting...\n")
                                                                     
 #Re-run sbatch using new mem-per-cpu and time override
-cmd_override <- paste0("sbatch --mem-per-cpu=",newCoreMem,
+cmd_override <- paste0("sbatch --mem-per-cpu=",optMem_perCore,
                        " --time=",optTime,
-                       " -c=",adjustCores,
+                       " -c ",adjustCores,
                        " permuLite_run.sh")
 sink("../processed/opt_params.txt")
 cat(cmd_override)
