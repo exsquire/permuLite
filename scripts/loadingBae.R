ecf0e020a42f5d2a8fa69b850790a10ee2b45c20
Excel Que <34141973+exsquire@users.noreply.github.com>
2020-01-03 11:31:31 -0800

    Create loadingBae.R

---
+++ scripts/loadingBae.R
@@ -0,0 +1,21 @@
+#LoadingBae for permuLite
+
+loadingBae <- function(path, key){
+  pathIn <- list()
+  for(i in seq_along(key)){
+    pathIn[[key[i]]] = list.files(path = path,
+                                  pattern = key[i],
+                                  full.names = TRUE,
+                                  ignore.case = TRUE)
+  }
+
+  #Check
+  if(!all(sapply(pathIn, function(x) length(x) > 0))){
+    cat("Error: One or more input(s) could not be found.\n")
+    print(sapply(pathIn, function(x) length(x) > 0))
+  }else{
+    cat("\nFound the following inputs: \n")
+    print(sapply(pathIn, function(x) length(x) > 0))
+  }
+  return(pathIn)
+}
