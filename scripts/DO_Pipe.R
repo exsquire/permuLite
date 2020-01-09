72b44019b4988c50b77177481d95fa4882158fee
Excel Que <34141973+exsquire@users.noreply.github.com>
2020-01-09 11:56:12 -0800

    Update DO_Pipe.R

--- scripts/DO_Pipe.R
+++ scripts/DO_Pipe.R
@@ -1,22 +1,43 @@
 #DO_PIPE
-dir.create("../output")
-dir.create("../output/data")
-dir.create("../output/plots")
-dir.create("../output/plots/lod/")
+setwd(".")
+#----------------
+library(qtl2)
+library(dplyr)
+library(httr)
+library(enrichR)
+
+dir.create("../output/data/peak-genes", recursive = TRUE)
+dir.create("../output/data/enrichr-results")
+dir.create("../output/plots/lod/", recursive = TRUE)
 
 #Select threshold level: 1 = 63% (associative), 2 = 95% (significant), 3 = 99% (highly significant)
 #If different thresholds are required, they can be generated from the full permutation matrix
 
-prefix <- "DOWL_addCov_Raw"
+
+cat("\nInput run name...\n")
+prefix <- readLines(file("stdin"),n=1)
+
+#Local testing only-----
+#prefix <- "mixedCovar_Raw"
+#stopifnot(!grepl(" ", prefix))
+#-----------------------
+saveRDS(prefix, "../processed/prefix.rds")
+
+cat("Loading Autosomal Permutation Matrix...\n")
 permMat <- readRDS("../processed/permuLite_matrix_A.rds")
 permThresh <- apply(permMat, 2, quantile, probs = c(0.63, 0.9, 0.95, 0.99))
+cat("Loading Physical Map...\n")
 pmap <- readRDS(list.files(path = "../test/", pattern = "map", full.names = T))
 
 
+#Result File Generation + Permutation Thresholding----------------
+cat("Loading Genome Scan Outfile...\n")
 scanOut <- readRDS("../processed/testOut.rds")
 #IMPORTANT: align the order of the permThresh columns with the scanOut columns
 permThresh <- permThresh[,match(colnames(scanOut), colnames(permThresh))]
 stopifnot(colnames(permThresh) == colnames(scanOut))
+
+cat("Writing out resfiles...\n")
 for(i in 1:nrow(permThresh)){
   desig <- paste0("signif_",gsub("%","",rownames(permThresh)[i]))
   thresh = permThresh[i,]
@@ -33,9 +54,8 @@ for(i in 1:nrow(permThresh)){
   saveRDS(peaks, file = paste0("../output/data/", prefix, "_peaks_",desig,".rds"))
 }
 
-
-
-#Analyze the 63 quantile peak file 
+#Visualization-----------------------------------------------------
+#Analyze the 63rd (associative) quantile peak file 
 peaks <- readRDS(paste0("../output/data/",prefix,"_peaks_signif_63.rds"))
 #Remove x chromosome peaks, then loop plot from the outfile
 peaks_noX <- peaks[peaks$chr != "X",]
@@ -67,10 +87,14 @@ for(i in 1:nrow(peaks_noX)){
 }
 close(pb)
 cat("Generating Global Peak Summary Plot...")
+
+#For this type of plot, x-axis never changes, but y-axis needs around 67 pixels per row to look good
+estHeight <- length(unique(peaks_noX$lodcolumn)) * 67
+estWidth <- estHeight * 1.5
 png(paste0("../output/plots/lod/",prefix,"_peakSummary",".png"),
     res = 300,
-    height = 2400,
-    width = 3000)
+    height = estHeight,
+    width =  estWidth)
 par(mar = c(4,15,4,2))
 plot_peaks(peaks_noX, 
            map = pmap, 
@@ -79,7 +103,101 @@ plot_peaks(peaks_noX,
            cex.lab = 0.5,
            tick.height = 0.8,
            gap = 0, 
-           main = "Global Peak Summary",
+           main = paste0("Global Peak Summary - ",prefix),
            alt = "white")
 box()
 dev.off()
+
+#Gene Query-------------------------------------------------------
+#Check if "mouse_genes.sqlite" in the test folder, if not, download it
+#wget is twice as fast when called from .sh compared to Rscript
+system("chmod 755 getMouseGenes.sh")
+system("./getMouseGenes.sh")
+
+#MGI Database lookup - save RDSes of all genes within the CI of all peaks
+#Submit peaks with CI - trim to the desired top x candidates
+#Open a connection to the SQLdb
+con <- dbConnect(SQLite(), "../test/mouse_genes.sqlite")
+genes <- tbl(con, "genes")
+stopifnot(nrow(peaks_noX) >= 10)
+inPeaks <- top_n(peaks_noX, 10, lod)
+for(i in 1:nrow(inPeaks)){
+  tmp <- inPeaks[i,]
+  tmp_chr <- as.character(tmp$chr)
+  tmp_start <- tmp$ci_lo
+  tmp_stop <- tmp$ci_hi
+  desig <- paste0("peak",i,"-",tmp$lodcolumn,"-",
+                  "chr",tmp$chr,
+                  "-",floor(tmp$ci_lo),
+                  "-",ceiling(tmp$ci_hi))
+  if(tmp$pos < tmp_start | tmp$pos > tmp_stop){
+    stop("Whoa there, marker isn't within CI!")
+  }
+  lookUp <- genes %>%  
+    filter(chr == tmp_chr &
+             type == "gene" &
+             source == "MGI" &  
+             start > tmp_start*10^6 &
+             stop < tmp_stop*10^6) %>% #Get your rows
+    dplyr::select(-ID, 
+                  -score, 
+                  -phase, 
+                  -Parent, 
+                  -Dbxref, 
+                  -gene_id) %>% #Get your columns
+    collect()
+  
+  lookUp <- lookUp[!grepl("^Gm",lookUp$Name),]
+  lookUp <- lookUp[!grepl("riken",tolower(lookUp$description)),]
+  if(nrow(lookUp) != 0){
+    saveRDS(lookUp, 
+            file =paste0("../output/data/peak-genes/",
+                         desig,".rds"))
+  }
+}
+#Close database connection
+dbDisconnect(con)
+
+
+#EnrichR Query-------------------------------------------------------
+dbs <- as.character(listEnrichrDbs()[["libraryName"]])
+dbNonGrata <- c("GeneSigDB",
+                "GTEx_Tissue_Sample_Gene_Expression_Profiles_down",
+                "GTEx_Tissue_Sample_Gene_Expression_Profiles_up",  
+                "MSigDB_Computational",
+                "NIH_Funded_PIs_2017_AutoRIF_ARCHS4_Predictions",
+                "NIH_Funded_PIs_2017_GeneRIF_ARCHS4_Predictions",
+                "DisGeNET",
+                "SubCell_BarCode",
+                "Chromosome_Location_hg19",
+                "DSigDB",
+                "Data_Acquisition_Method_Most_Popular_Genes",
+                "Allen_Brain_Atlas_up",
+                "Old_CMAP_up",
+                "Old_CMAP_down"
+)
+dbs <- dbs[!dbs %in% dbNonGrata] #errors with dbGaP and 
+#Loop through each file in peakGenes, query enrichr
+peakCount <- length(list.files("../output/data/peak-genes/"))
+peakFiles <- list.files("../output/data/peak-genes/", 
+                        full.names = TRUE)
+cat("Establishing connection...\n")
+cat("Querying",peakCount,"QTL...\n")
+pb <- txtProgressBar(min = 0, 
+                     max = peakCount, 
+                     style = 3)
+for(i in seq_along(peakFiles)){
+  tmp <- readRDS(peakFiles[i])
+  lab <- paste0(gsub(".rds","",
+                     basename(peakFiles[i])),"_enrich.rds")
+  genes <- tmp$Name
+  enriched <- enrichrMod(genes, dbs, quiet = TRUE)
+  if(!is.null(enriched)){
+    saveRDS(enriched, file = paste0("../output/data/enrichr-results/",lab))
+  }
+  setTxtProgressBar(pb, i)
+}
+close(pb)
+
+
+
